1c534f89d631c49bf2202b0b64d2c515ea099f17
itzyashh <yashjadhav1411@rocketmail.com>
2022-12-14 09:23:22 +0530

    feat: add Navigator

---
+++ StackNavigator.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import HomeScreen from "./src/screens/HomeScreen"
+
+const Stack = createNativeStackNavigator()
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  )
+}
